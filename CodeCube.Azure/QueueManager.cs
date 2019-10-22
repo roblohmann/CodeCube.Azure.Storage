410c806e642d8645137a8dea3062d3eb8db24a51
Rob Lohmann <roblohman@gmail.com>
2019-10-22 13:12:34 +0200

    added ability to connect to queuemanager

--- CodeCube.Azure/QueueManager.cs
+++ CodeCube.Azure/QueueManager.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using CodeCube.Azure.Constants;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace CodeCube.Azure
 {
     public sealed class QueueManager : BaseManager
     {
         private readonly string _queuename;
+        private CloudQueue _queue;
 
         internal QueueManager(string connectionstring, string queuename) : base(connectionstring)
         {
@@ -22,5 +25,27 @@ namespace CodeCube.Azure
 
             _queuename = queuename;
         }
+
+        public CloudQueue Connect()
+        {
+            if(_queue == null) ConnectToQueue();
+            
+            return _queue;
+        }
+
+        #region privates
+        private void ConnectToQueue()
+        {
+            CloudStorageAccount storageAccount = ConnectCloudStorageAccountWithConnectionString();
+
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a queue.
+            _queue = queueClient.GetQueueReference(_queuename);
+
+            // Create the queue if it doesn't already exist
+            _queue.CreateIfNotExistsAsync();
+        }
+        #endregion
     }
 }
